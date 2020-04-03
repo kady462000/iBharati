b568c17d6dc83a37437652d65a73c84b4990e920
kady462000 <kady462000@gmail.com>
2020-04-03 17:03:01 +0530

    Added courosel

---
+++ assets/js/couroselscript.js
@@ -0,0 +1,9 @@
+
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+     $('.carousel').carousel();
+    });
+
+</script>
